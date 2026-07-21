82bd0f3e5aea1bfc41f0b7c8d9c9983d5273382c
rukio <ruki000.100.001.101@gmail.com>
2026-07-21 23:41:53 +0900

    rgb to hsv

--- index.js
+++ index.js
@@ -26,9 +26,30 @@ function reload() {
     const src = ctx.getImageData(0, 0, dw, dh);
     const dst = [];
     for (let i = 0; i < src.data.length; i += 4) {
-        dst.push(Math.trunc(src.data[i + 0] / 255 * 100));
-        dst.push(Math.trunc(src.data[i + 1] / 255 * 100));
-        dst.push(Math.trunc(src.data[i + 2] / 255 * 100));
+        const r = src.data[i + 0];
+        const g = src.data[i + 1];
+        const b = src.data[i + 2];
+        const max = Math.max(r, g, b);
+        const min = Math.min(r, g, b);
+        const mm = max - min;
+        let h = 0;
+        if (mm !== 0) {
+            if (r > g && r > b) {
+                h = (g - b) / mm / 6;
+            } else if (g > r && g > b) {
+                h = 1 / 3 + (b - r) / mm / 6;
+            } else {
+                h = 2 / 3 + (r - g) / mm / 6;
+            }
+        }
+        let s = 0;
+        if (max !== 0) {
+            s = mm / max * 100;
+        }
+        const v = max / 255 * 100;
+        dst.push(Math.trunc(h));
+        dst.push(Math.trunc(s));
+        dst.push(Math.trunc(v));
     }
 
     pixelDataOutput.value = dst.toString();
